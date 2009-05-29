c5d8e997f67f1ac25430cc51271b4e7011e3826a
Jeroen Tietema <jeroen@tietema.net>
2009-05-29 11:32:55 +0200

    #11 license added

--- parts.py
+++ parts.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of RCr and copyright (C) Maik Gosenshuis and 
+# Jeroen Tietema 2008-09.
+#
+# RCr is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RCr is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RCr.  If not, see <http://www.gnu.org/licenses/>.
+
 options = {
     'body': {
         'fellali': {
